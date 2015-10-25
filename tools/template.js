c7038990a4eb1e5b519a22c4911c81caab0cb982
David Storey <david@lostincode.co.uk>
2015-10-25 16:06:30 +0000

    Adding loops to template engine

--- tools/template.js
+++ tools/template.js
@@ -3,14 +3,17 @@
 ;(function() {
 	'use strict';
 
-	var Template = function(template, model) {
+	// TODO Add handling for conditional statements
+	// TODO Strip spaces
+
+	var Template = function(template) {
 		this.template = template;
 
 		return this;
 	};
 
-	Template.prototype.prefix = '{{';
-	Template.prototype.suffix = '}}';
+	Template.prototype.prefix = '<!--';
+	Template.prototype.suffix = '-->';
 
 	Template.prototype.render = function(data) {
 		var output = this.template;
@@ -18,11 +21,33 @@
 
 		while (output.indexOf(this.prefix) > -1) {
 			var key = output.substring(output.indexOf(this.prefix) + this.prefix.length, output.indexOf(this.suffix));
-			var value = getByString(data, key);
-			if (typeof(value) === 'function') {
-				value = value.call(data);
+
+			// Handle each statements
+			if (key.indexOf('#each') > -1) {
+				var openingTag = this.prefix + key + this.suffix;
+				var closingTag = this.prefix + '/each' + this.suffix;
+
+				var original = output.substring(output.indexOf(openingTag) + openingTag.length);
+				original = original.substring(0, original.indexOf(closingTag));
+
+				var replacement = '';
+
+				var bob = new Template('' + original);
+				var arrayName = key.split(' ')[1];
+				var array = data[arrayName];
+
+				for (var x = 0; x < array.length; x++) {
+					replacement += bob.render(array[x]);
+				}
+
+				output = output.replace(openingTag + original + closingTag, replacement);
+			} else { // Handle normal variables
+				var value = getByString(data, key);
+				if (typeof(value) === 'function') {
+					value = value.call(data);
+				}
+				output = output.replace(this.prefix + key + this.suffix, value);
 			}
-			output = output.replace(this.prefix + key + this.suffix, value);
 		}
 
 		return output;
